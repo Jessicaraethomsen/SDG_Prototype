f36af207e6225d60c115c5d6bae9e5ae1900c8dd
Jessica Thomsen <>
2018-11-18 22:10:07 +0100

    Function: Local storage working

--- js/storename.js
+++ js/storename.js
@@ -1,27 +1,63 @@
+
+function resetname() {
+		localStorage.clear();	
+	}
+
 function setName() {
- var playername = localStorage.getItem('user');
+ 		var playername = localStorage.getItem('user');
 		this.name = playername;
-		this.score = localStorage.getItem('userScore');
-		document.getElementById("player").innerHTML = this.name;
+		document.getElementById("playergirl").innerHTML = this.name;
+
+		var playernameboy = localStorage.getItem('userboy');
+		this.nameboy = playernameboy;
+		
+		document.getElementById("playerboy").innerHTML = this.nameboy;
 }
 
- function myFunction() {
+ function myGirl() {
 
 		// prepare client storage for user + score if doesn't exist
 		if (!localStorage.getItem('user')) {
 			localStorage.setItem('user', '');
-			localStorage.setItem('userScore', 0);
+			
 		}
 		// prompt for user name if not yet set
 		if (localStorage.getItem('user') === '') {
-			this.name = prompt("Please enter your name", "Mr. X");
-			// set username
+			this.name = "Sally";
 			localStorage.setItem('user', this.name);
 		}
+
+
 		// get player name from local storage
 		var playername = localStorage.getItem('user');
 		this.name = playername;
-		this.score = localStorage.getItem('userScore');
-		alert('Hi ' + this.name + ', your all-time score is ' + this.score + '.  Ready to roll?');
-		document.getElementById("player").innerHTML = this.name;
-	}
+		alert('Hi ' + this.name + ' Are you Ready to Play');
+		document.getElementById("playergirl").innerHTML = this.name;
+		localStorage.setItem('userboy', '');
+		}
+		
+
+
+
+
+ function myBoy() {
+
+		// prepare client storage for user + score if doesn't exist
+		if (!localStorage.getItem('userboy')) {
+			localStorage.setItem('userboy', '');
+			
+		}
+		// prompt for user name if not yet set
+		if (localStorage.getItem('userboy') === '') {
+			this.nameboy = "Walter";
+			localStorage.setItem('userboy', this.nameboy);
+		}
+		// get player name from local storage
+		var playernameboy = localStorage.getItem('userboy');
+		this.nameboy = playernameboy;
+		alert('Hi ' + this.nameboy + ' Are you Ready to Play');
+		document.getElementById("playerboy").innerHTML = this.nameboy;
+		localStorage.setItem('user', '');
+
+		
+}
